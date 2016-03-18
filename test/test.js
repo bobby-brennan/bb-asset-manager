8e44403b3c43aa0c036380eda964927fbea08677
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-18 01:27:07 +0000

    add cacheID

--- test/test.js
+++ test/test.js
@@ -6,21 +6,24 @@ var AssetMan = require('../index.js');
 describe('AssetManager', function() {
   var JS_ASSETS = ['a.js', 'b.js'];
   var CSS_ASSETS = ['a.css', 'b.css'];
-  var DEFAULT_OPTS = {
-    staticDirectory: __dirname,
-    minify: false,
-    js: {
-      inputDirectory: 'assets/js',
-      outputDirectory: 'golden/js',
-    },
-    css: {
-      inputDirectory: 'assets/css',
-      outputDirectory: 'golden/css',
-    },
+  var defaultOpts = function(overrides) {
+    var opts = {
+      staticDirectory: __dirname,
+      minify: false,
+      js: {
+        inputDirectory: 'assets/js',
+        outputDirectory: 'golden/js',
+      },
+      css: {
+        inputDirectory: 'assets/css',
+        outputDirectory: 'golden/css',
+      },
+    }
+    return _.extend(opts, overrides || {});
   }
 
   var setup = function(opts) {
-    opts = opts || DEFAULT_OPTS;
+    opts = defaultOpts(opts);
     var assetMan = new AssetMan(opts);
     assetMan.addAsset('all', {
       js: JS_ASSETS,
@@ -55,38 +58,39 @@ describe('AssetManager', function() {
     var assetMan = setup();
     Expect(assetMan.getAssetTags('all')).to.equal(
       getCSS('/golden/css/all.css') + getJS('/golden/js/all.js'));
+  });
+  if('should render cacheID', function() {
+    var assetMan = setup({cacheID: 3});
+    Expect(assetMan.getAssetTags('all')).to.equal(
+      getCSS('/golden/css/all.css?cacheID=3') + getJS('/golden/js/all.js?cacheID=3'));
   })
   it('should compile', function() {
     var assetMan = setup();
     assetMan.compile();
   });
   it('should compile minified', function() {
-    var opts = JSON.parse(JSON.stringify(DEFAULT_OPTS));
-    opts.minify = true;
+    var opts = defaultOpts({minify: true});
     opts.js.outputDirectory += '_min';
     opts.css.outputDirectory += '_min';
     var assetMan = setup(opts);
     assetMan.compile();
   })
   it('should render original assets', function() {
-    var opts = JSON.parse(JSON.stringify(DEFAULT_OPTS));
-    opts.useOriginalAssets = true;
-    var assetMan = setup(opts);
+    var assetMan = setup({useOriginalAssets: true});
     Expect(assetMan.getJSTags('all')).to.equal(getJS('/assets/js/a.js') + '\n' + getJS('/assets/js/b.js'));
     Expect(assetMan.getJSTags('reverse')).to.equal(getJS('/assets/js/b.js') + '\n' + getJS('/assets/js/a.js'));
   });
   it('should allow language override', function() {
-    var opts = JSON.parse(JSON.stringify(DEFAULT_OPTS));
-    opts.useOriginalAssets = true;
+    var opts = defaultOpts({
+      useOriginalAssets: true,
+    });
     opts.js.useOriginalAssets = false;
     var assetMan = setup(opts);
     Expect(assetMan.getJSTags('all')).to.equal(getJS('/golden/js/all.js'));
     Expect(assetMan.getCSSTags('all')).to.equal(getCSS('/assets/css/a.css') + '\n' + getCSS('/assets/css/b.css'));
   });
   it('should allow basePath', function() {
-    var opts = JSON.parse(JSON.stringify(DEFAULT_OPTS));
-    opts.basePath = '/base';
-    var assetMan = setup(opts);
+    var assetMan = setup({basePath: '/base'});
     Expect(assetMan.getJSTags('all')).to.equal(getJS('/base/golden/js/all.js'));
     Expect(assetMan.getCSSTags('all')).to.equal(getCSS('/base/golden/css/all.css'));
   })
